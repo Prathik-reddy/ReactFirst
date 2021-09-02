b1351b9d04a642d594bcea27ac3b77e9d910b4df
Prathik Reddy <reddyprathik1002@gmail.com>
2021-09-02 20:51:47 +0530

    added useState and made a Uppercase button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar'
+import Textform from './components/Textform'
 function App() {
   return(
     <>
     <Navbar title="TextUtils" About="AboutUs"/>
     {/* <Navbar /> */}
+    <div className="container my-3">
+    <Textform heading="Enter the text to analyze below"/>
+    </div>
     </>
   );
 }
