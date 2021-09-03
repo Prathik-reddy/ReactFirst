9474f5a16f9f64f591534c26eb0c7f9a296d17fc
Prathik Reddy <reddyprathik1002@gmail.com>
2021-09-03 21:21:56 +0530

    added about us part

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
-import Navbar from './components/Navbar'
-import Textform from './components/Textform'
+import About from './components/About';
+import Navbar from './components/Navbar';
+import Textform from './components/Textform';
+
 function App() {
   return(
     <>
@@ -8,6 +10,7 @@ function App() {
     {/* <Navbar /> */}
     <div className="container my-3">
     <Textform heading="Enter the text to analyze below"/>
+    <About/>
     </div>
     </>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,16 +13,12 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item active">
-              <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+              <a className="nav-link" href="/">Home</a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="/">{props.About}</a>
             </li>
           </ul>
-          <form className="form-inline my-2 my-lg-0">
-            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
         </div>
       </nav>
     )
@@ -31,6 +27,6 @@ export default function Navbar(props) {
 Navbar.propTypes= {title: PropTypes.string.isRequired,
 About: PropTypes.string.isRequired}
 
-// Should always declare a defaultProps section 
+// Should always declare a defaultProps section
 Navbar.defaultProps= {title:"Set title here",
 About : "Set aboutText here"}
