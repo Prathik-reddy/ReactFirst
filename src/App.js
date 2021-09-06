8e4c81bc5f6cf0228925bb34a3899e1a3f2f5ab2
Prathik Reddy <tvkp333555@gmail.com>
2021-09-06 19:18:17 +0530

    added basic routing in the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
 import React, { useState } from 'react'
 import Alert from './components/Alert';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   const [Mode,setMode]=useState("light"); //whether dark mode is enabled or not
@@ -24,22 +30,36 @@ function App() {
       setMode("light");
       document.body.style.backgroundColor="white";
       showAlert("Light mode enabled","success");
+      document.title = "TextUtils : Light mode ";
     }
     else{
     setMode("dark");
     document.body.style.backgroundColor="#11033d";
     showAlert("Dark mode enabled","success");
+    document.title = "TextUtils : Dark mode ";
     }
   }
   return(
     <>
+    <Router>
     <Navbar title="TextUtils" About="AboutUs" mode={Mode} toggleMode={toggleMode}/>
     {/* <Navbar /> */}
     <Alert alert={alert}/>
     <div className="container my-3">
-    <Textform showAlert={showAlert} heading="Enter the text to analyze below"  mode={Mode}/>
-    {/* <About/> */}
+    <Switch>
+          <Route exact path="/about">
+            {/* exact path matches the exact path . Not mentioning the exact keyword may cause the react app to partially match the components .
+            Eg /user --> component1
+            /user/home --> component2 .
+            Here the react app may render the other component if exact keyword is not mentioned*/}
+            <About />
+          </Route>
+          <Route exact path="/">
+            <Textform showAlert={showAlert} heading="Enter the text to analyze below"  mode={Mode}/>
+          </Route>
+    </Switch>
     </div>
+    </Router>
     </>
   );
 }
