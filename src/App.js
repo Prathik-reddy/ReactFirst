da0976d73d0e0feac0d57bef241b9777404c5873
Prathik Reddy <tvkp333555@gmail.com>
2021-09-09 16:41:47 +0530

    add color palette

--- src/App.js
+++ src/App.js
@@ -24,7 +24,17 @@ function App() {
     }, 2000);
   }
 
-  const toggleMode = ()=>{
+  const  removeBodyClasses =()=> {
+    document.body.classList.remove("bg-light");
+    document.body.classList.remove("bg-dark");
+    document.body.classList.remove("bg-danger");
+    document.body.classList.remove("bg-warning");
+    document.body.classList.remove("bg-success");
+  }
+  const toggleMode = (cls)=>{
+    removeBodyClasses();
+    console.log(cls);
+    document.body.classList.add("bg-"+cls);
     if (Mode === "dark"){
       setMode("light");
       document.body.style.backgroundColor="white";
